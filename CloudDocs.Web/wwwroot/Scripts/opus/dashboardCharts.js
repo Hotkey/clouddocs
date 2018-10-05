228f2175874e1325a781c4166bea92ca20ca45dc
Faruk ALKAYA <farukalkaya@gmail.com>
2018-10-05 17:55:07 +0300

    new challenge

---
+++ CloudDocs.Web/wwwroot/Scripts/opus/dashboardCharts.js
@@ -0,0 +1,283 @@
+﻿
+
+// ------------------------------
+$(window).on("load", function () {
+    // Toplam Elektrik Tüketimi (kWh) > Column stacked chart
+    LoadElectricTotalKwh();
+    // Toplam enerji gideri (TL)
+    LoadTotalEnergyChart();
+
+    LoadAllEnergyTypesLineChart();
+});
+
+
+function LoadTotalEnergyChart() {
+    var n = c3.generate({
+        bindto: "#total-energy-amount",
+        size: {
+            height: 400
+        },
+        color: {
+            pattern: ["#673AB7",
+                "#E91E63"]
+        },
+        data: {
+            columns: [["TL",
+                0,
+                850059,
+                820021,
+                762000,
+                692039,
+                853000,
+                783992
+                ]]
+        },
+        grid: {
+            x: {
+                lines: [{
+                    value: 1,
+                    text: "Ocak"
+                },
+                {
+                    value: 2,
+                    text: "Şubat"
+                },
+                {
+                    value: 3,
+                    text: "Mart"
+                },
+                {
+                    value: 4,
+                    text: "Nisan"
+                },
+                {
+                    value: 5,
+                    text: "Mayıs"
+                },
+                {
+                    value: 6,
+                    text: "Haziran"
+                }]
+            }
+        }
+    });
+    $(".menu-toggle").on("click",
+        function () {
+            n.resize()
+        });
+}
+
+function LoadElectricTotalKwh() {
+    // Get the context of the Chart canvas element we want to select
+    var ctx = $("#total-electric-kwh");
+    // Chart Options
+    var chartOptions = {
+        title: {
+            display: false,
+            text: "Toplam Elektrik Tüketimi (kWh)"
+        },
+        tooltips: {
+            mode: 'label'
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        responsiveAnimationDuration: 500,
+        scales: {
+            xAxes: [{
+                stacked: true,
+                display: true,
+                gridLines: {
+                    color: "#f3f3f3",
+                    drawTicks: false,
+                },
+                scaleLabel: {
+                    display: true,
+                }
+            }],
+            yAxes: [{
+                stacked: true,
+                display: true,
+                gridLines: {
+                    color: "#f3f3f3",
+                    drawTicks: false,
+                },
+                scaleLabel: {
+                    display: true,
+                }
+            }]
+        }
+    };
+
+    // Chart Data
+    var chartData = {
+        labels: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran"],
+        datasets: [{
+            label: "Elektrik Şebeke Tüketimi (kWh)",
+            data: [65, 59, 80, 81, 56, 93],
+            backgroundColor: "#5175E0",
+            hoverBackgroundColor: "rgba(81,117,224,.8)",
+            borderColor: "transparent"
+        }, {
+            label: "Elektrik Kojen Üretimi (Kwh)",
+            data: [28, 48, 40, 19, 86, 45],
+            backgroundColor: "#28D094",
+            hoverBackgroundColor: "rgba(22,211,154,.8)",
+            borderColor: "transparent"
+        }]
+    };
+
+    var config = {
+        type: 'bar',
+        // Chart Options
+        options: chartOptions,
+        data: chartData
+    };
+
+    // Create the chart
+    var lineChart = new Chart(ctx, config);
+}
+
+function LoadAllEnergyTypesLineChart() {
+    var t = c3.generate({
+        bindto: "#energy-types-line",
+        size: {
+            height: 400
+        },
+        point: {
+            r: 4
+        },
+        color: {
+            pattern: ["#673AB7", "#E91E63", "#E1E6E4"]
+        },
+        data: {
+            columns: [["Elektrik Gideri (TL)",
+                30,
+                200,
+                100,
+                400,
+                150,
+                250],
+            ["Doğalgaz Gideri (TL)",
+                50,
+                20,
+                10,
+                40,
+                15,
+                25],
+            ["Su Gideri (TL)",
+                    10,
+                    12,
+                    9,
+                    13,
+                    10,
+                    11]]
+        },
+        grid: {
+            y: {
+                show: !0,
+                stroke: "#ff0"
+            }
+        }
+    });
+
+    //setTimeout(function () {
+    //    t.load({
+    //        columns: [["Elektrik",
+    //            230,
+    //            190,
+    //            300,
+    //            500,
+    //            300,
+    //            400]]
+    //    })
+    //}, 1e3);
+
+    //setTimeout(function () {
+    //    t.load({
+    //        columns: [["Doğalgaz",
+    //            130,
+    //            150,
+    //            200,
+    //            300,
+    //            200,
+    //            100]]
+    //    })
+    //}, 1500);
+
+    //setTimeout(function () {
+    //    t.unload({
+    //        ids: "Elektrik"
+    //    })
+    //}, 2e3);
+
+        $(".menu-toggle").on("click",
+            function () {
+                t.resize()
+            });
+}
+
+
+!function (a, e, r) {
+    "use strict";
+    for (var t = [
+        [],
+        [],
+        [],
+        []
+    ], i = new Rickshaw.Fixtures.RandomData(150), n = 0; n < 150; n++) i.addData(t);
+    var o = r("#live-graphs"),
+        s = new Rickshaw.Graph({
+            element: o.get(0),
+            width: o.width(),
+            height: 400,
+            renderer: "area",
+            stroke: !0,
+            series: [{
+                color: "#99B898",
+                data: t[0],
+                name: "Acıbadem Üni."
+            }, {
+                color: "#FECEA8",
+                data: t[1],
+                    name: "Altunizade Has."
+            }, {
+                color: "#FF847C",
+                data: t[2],
+                    name: "Maslak Has."
+            }, {
+                color: "#6C5B7B",
+                data: t[3],
+                    name: "International Has."
+            }]
+        });
+    s.render(), setInterval(function () {
+        i.removeData(t), i.addData(t), s.update()
+    }, 2e3);
+    new Rickshaw.Graph.HoverDetail({
+        graph: s
+    }), new Rickshaw.Graph.Behavior.Series.Toggle({
+        graph: s
+    });
+    r(a).on("resize", function () {
+        s.configure({
+            width: o.width()
+        }), s.render()
+    });
+    var d = r("#live-visits-doughnut");
+    new Chart(d, {
+        type: "doughnut",
+        options: {
+            responsive: !0,
+            maintainAspectRatio: !1,
+            responsiveAnimationDuration: 500
+        },
+        data: {
+            labels: ["Acıbadem Üni.", "Altunizade Has.", "Maslak Has.", "International Has."],
+            datasets: [{
+                label: "Giderler",
+                data: [154684, 145300, 142130, 134500],
+                backgroundColor: ["#99B898", "#FECEA8", "#FF847C", "#6C5B7B"]
+            }]
+        }
+    })
+}(window, document, jQuery);
---
+++ CloudDocs.Web/wwwroot/assets/js/opus.js
@@ -0,0 +1,246 @@
+﻿
+$.ajaxSetup({
+    statusCode: {
+        401: function () {
+            location.href = "/Account/Login";
+        }
+    }
+});
+function AjaxPost(url, data, fnSuccess, fnError) {
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        cache: false,
+        async: true,
+        success: fnSuccess,
+        error: fnError
+    });
+};
+
+function block() {
+    $.blockUI({
+        message: '<h1><img src="/app-assets/images/icons/spinner.gif" /> Lütfen bekleyiniz...</h1>',
+        css: { border: '0px' },
+        overlayCSS: {
+            backgroundColor: '#000',
+            opacity: 0.3,
+            cursor: 'wait'
+        },
+    });
+}
+
+function errorMessage(msg) {
+    toastr.error(msg, '', {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "6000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    });
+}
+
+function successMessage(msg) {
+    toastr.success(msg);
+}
+
+function unBlock() {
+    $.unblockUI();
+}
+
+function unBlockWithSuccessMessage(msg) {
+    $.unblockUI();
+    successMessage(msg);
+}
+
+function unBlockWithErrorMessage(msg) {
+    $.unblockUI();
+    errorMessage(msg);
+}
+
+function FillSelect(elm, data) {
+    $(elm).empty();
+    $(elm).append("<option value=''>Seçiniz</option>")
+    for (var i = 0; i < data.length; i++) {
+        $(elm).append("<option " + (data[i].Selected ? "selected=\"selected\"" : "selected") + " value='" + data[i].Value + "'>" + data[i].Text + "</option>");
+    }
+    $(elm).trigger('change');
+}
+
+$('#ddlDivisions').change(function () {
+    var divisionId = $(this).val();
+    if (divisionId != undefined && divisionId != '')
+        AjaxPost('/GenericForm/GetDivisionLocation', { divisionId: divisionId }, ddlLocationSuccess, ddlLocationError)
+});
+
+function ddlLocationSuccess(result) {
+    FillSelect($('#ddlDivisionLocations'), result);
+}
+
+function ddlLocationError(error) {
+    errorMessage('Hata oluştu ' + error);
+}
+
+$('#ddlEnergyInOutType').change(function () {
+    var inOutTypeId = $(this).val();
+    if (inOutTypeId == 1) {
+        $('#dvConsumption').show();
+        $('#dvAmount').hide();
+    } else {
+        $('#dvConsumption').hide();
+        $('#dvAmount').show();
+    }
+    if (inOutTypeId != undefined && inOutTypeId != '')
+        AjaxPost('/GenericForm/GetEnergySubTypes', { inOutTypeId: inOutTypeId }, ddlEnergyInOutTypeSuccess, ddlEnergyInOutTypeError)
+});
+
+function ddlEnergyInOutTypeSuccess(result) {
+    FillSelect($('#ddlEnergySubTypes'), result);
+}
+
+function ddlEnergyInOutTypeError(error) {
+    errorMessage('Hata oluştu ' + error);
+}
+
+$("#btnElectricForm").click(function () {
+    block();
+    var energyoutInType = $('#ddlEnergyInOutType').val();
+    var model = {
+        LocationID: $('#ddlDivisionLocations').val(),
+        EnergySubTypeID: $('#ddlEnergySubTypes').val(),
+        EnergyInOutTypeID: energyoutInType ,
+        MeasuredValue: energyoutInType == '1' ? $('#txtConsumption').val() : $('#txtAmount').val(),
+        Description: $('#txtDescription').val(),
+        Year: $('#ddlYears').val(),
+        Month: $('#ddlMonths').val()
+    };
+
+    AjaxPost('/GenericForm/SaveEnergyForm', { model: model }, SaveEnergyFormSuccess, SaveEnergyFormError)
+});
+
+function SaveEnergyFormSuccess(result) {
+    if (!result.HasError) {
+        unBlockWithSuccessMessage('İşleminiz başarılı olarak gerçekleştirildi');
+        $('#ddlDivisions').val('');
+        $('#ddlDivisions').change();
+        $('#ddlDivisionLocations').val('');
+        $('#ddlDivisionLocations').change();
+        $('#ddlEnergySubTypes').val('');
+        $('#ddlEnergySubTypes').change();
+        $('#ddlEnergyInOutType').val('');
+        $('#ddlEnergyInOutType').change();
+        $('#txtDescription').val('');
+        $('#txtAmount').val('');
+        $('#txtConsumption').val('');
+        $('#ddlYears').val('');
+        $('#ddlYears').change();
+        $('#ddlMonths').val('');
+        $('#ddlMonths').change();
+    }
+    else {
+        unBlockWithErrorMessage(result.ErrorMessage);
+    }
+}
+
+function SaveEnergyFormError(error) {
+    unBlockWithErrorMessage('Hata oluştu : ' + error);
+}
+
+$("#btnListElectricDashboard").click(function () {
+    block();
+    setTimeout(function () {
+        unBlockWithSuccessMessage('İşleminiz tamamlandı');
+    }, 6000);
+});
+
+function display_kendoui_grid_error(e) {
+    if (e.errors) {
+        if ((typeof e.errors) == 'string') {
+            //single error
+            //display the message
+            alert(e.errors);
+        } else {
+            //array of errors
+            //source: http://docs.kendoui.com/getting-started/using-kendo-with/aspnet-mvc/helpers/grid/faq#how-do-i-display-model-state-errors?
+            var message = "The following errors have occurred:";
+            //create a message containing all errors.
+            $.each(e.errors, function (key, value) {
+                if (value.errors) {
+                    message += "\n";
+                    message += value.errors.join("\n");
+                }
+            });
+            //display the message
+            alert(message);
+        }
+        //ignore empty error
+    } else if (e.errorThrown) {
+        alert('Error happened');
+    }
+}
+
+function ToSeoUrl(url) {
+
+    // make the url lowercase         
+    var encodedUrl = url.toString().toLowerCase();
+
+    // replace & with and           
+    encodedUrl = encodedUrl.split(/\&+/).join("-and-")
+
+    // remove invalid characters 
+    encodedUrl = encodedUrl.split(/[^a-z0-9]/).join("-");
+
+    // remove duplicates 
+    encodedUrl = encodedUrl.split(/-+/).join("-");
+
+    // trim leading & trailing characters 
+    encodedUrl = encodedUrl.trim('-');
+
+    return encodedUrl;
+}
+
+function toDataURL(src, callback, outputFormat) {
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function () {
+        var canvas = document.createElement('CANVAS');
+        var ctx = canvas.getContext('2d');
+        var dataURL;
+        canvas.height = this.naturalHeight;
+        canvas.width = this.naturalWidth;
+        ctx.drawImage(this, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+    };
+    img.src = src;
+    if (img.complete || img.complete === undefined) {
+        img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        img.src = src;
+    }
+}
+
+// CSRF (XSRF) security
+function addAntiForgeryToken(data) {
+    //if the object is undefined, create a new one.
+    if (!data) {
+        data = {};
+    }
+    //add token
+    var tokenInput = $('input[name=__RequestVerificationToken]');
+    if (tokenInput.length) {
+        data.__RequestVerificationToken = tokenInput.val();
+    }
+    return data;
+};
